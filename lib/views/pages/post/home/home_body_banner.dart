322088e3615db5cd944e6be43791b8c48f97fae4
YouHyunKim <yuhyunk7@gmail.com>
2023-03-28 14:31:51 +0900

    homebodybanner add

--- lib/views/pages/post/home/home_body_banner.dart
+++ lib/views/pages/post/home/home_body_banner.dart
@@ -8,18 +8,29 @@ class HomeBodyBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.asset(
-            "assets/banner.jpg",
-            fit: BoxFit.cover,
-            width: double.infinity,
-            height: 320,
-          ),
-        ),
+        _bannerImage(),
+        Column(
+          children: [
+            Text("이제 여행은 가까운 곳에서", style: TextStyle(color: Colors.white),),
+            Text("새로운 공간에 머물러 보세요. 살아보기, 출장, 여행등 다양한 목적에 맞는 숙소를 찾아보세요", style: TextStyle(color: Colors.white),),
+            TextButton(onPressed: (){}, child: Text("가까운 여행지 둘러보기", style: TextStyle(color: Colors.white)),),
+          ],
+        )
 
 
       ],
     );
+  }
+
+  Widget _bannerImage() {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset(
+          "assets/banner.jpg",
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: 320,
+        ),
+      );
   }
 }
